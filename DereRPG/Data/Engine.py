bd452f2a8dcf04e9122d1bd17e474ef2b802f1e7
VoidTsundere <71775119+VoidTsundere@users.noreply.github.com>
2020-09-25 17:31:12 -0300

    Add files via upload

--- DereRPG/Data/Engine.py
+++ DereRPG/Data/Engine.py
@@ -1,9 +1,9 @@
-import json, os, random
+import json, os, random, time
 
 #Tables-----------------------
-lvlXp = [100,150,200,250]
-hpTable = [100,110,120,130]
-mpTable = [10,20,30,40]
+xpTable = [100,150,200,250,300,350,400,450,500,550,600]
+hpTable = [100,110,120,130,140,150,160,170,180,190,200]
+mpTable = [10,20,30,40,50,60,70,80,90,100]
 
 class System:
 	def NewSave(name):
@@ -32,10 +32,11 @@ class System:
 			"weapon1":0,
 			"subWeapon1":0,
 			"subWeapon2":0,
-			"xp":0
+			"xp":1000,
+			"debug":0
 			}
 		saveName = 'Saves/'+name+'.JDere'
-		jsonPlayerData = json.dumps(rawPlayerData, indent=2)
+		jsonPlayerData = json.dumps(rawPlayerData, indent=1)
 		openFile = open(saveName,'w')
 		openFile.write(jsonPlayerData)
 		openFile.close()
@@ -43,7 +44,7 @@ class System:
 	def LoadData(name):
 		saveName = 'Saves/'+name+'.JDere'
 		global rawData
-		with open(saveName, 'r') as rawData:
+		with open(saveName, 'r+') as rawData:
 			global pld
 			pld = json.load(rawData)
 			global nameVar
@@ -58,21 +59,38 @@ class System:
 			return 1
 
 	def levelUp():
-		if pld['xp'] >= lvlXp[pld['lvl']]:
+		if pld['xp'] >= xpTable[pld['lvl']]:
 			os.system('cls')
 			lvl = pld["lvl"] +1
 			oldHP = Player.Get.maxHp()
 			newHP = hpTable[lvl]
 			oldMP = Player.Get.maxMp()
 			newMP = mpTable[lvl]
+			#Update values----
+			VarPoints = Player.Get.upPoints() +1
+			Player.Update.upPoints(VarPoints)
+
+			VarXp = Player.Get.xp() -xpTable[Player.Get.lvl()]
+			Player.Update.xp(VarXp)
+
+			VarLvl = Player.Get.lvl() +1
+			Player.Update.lvl(VarLvl)
+
+			Player.Update.maxHp(newHP)
+			Player.Update.maxMp(newMP)
+
+			Player.Restore.hp('full')
+			Player.Restore.mp('full')
+			
+			#Update values----
 			upMessage = '\nAumentando o seu nível você obteve uma melhora de Hp de {oldHp} para {newHp}\ne tambem uma melhora de Mp de {oldMp} para {newMp}\n tambem recebeu 1 ponto de modificador que pode ser usado com /mod'.format(oldHp=oldHP,newHp=newHP,oldMp=oldMP,newMp=newMP)
 			return print(upMessage)
 		else:
 			return print('\nParece que você ainda não atende aos requeimentos necessários para subir de\nnível, volte denovo quando tiver XP suficiente pra isso')
 
-	class Round:
-		def checkStats():
-			if pld['xp'] >= lvlXp[pld['lvl']]:
+	class Check:
+		def stats():
+			if pld['xp'] >= xpTable[pld['lvl']]:
 				return print('Parece que você está pronto para subir de nível\nUse /lvlUp para passar para o próximo nível')
 
 class Player:
@@ -123,6 +141,38 @@ class Player:
 			return["subWeapon1"]
 		def subWeapon2():
 			return pld["subWeapon2"]
+		def xp():
+			return pld["xp"]
+
+	class Restore:
+		def hp(ammount):
+			addHealth=0
+			hpMax = Player.Get.maxHp()
+
+			if ammount == 'full':
+				addHealth = Player.Get.maxHp()
+			else:
+				addHealth = Player.Get.hp() + ammount
+
+			if addHealth < hpMax:
+				addHealth = Player.Get.maxHp()
+
+			Player.Update.hp(addHealth)
+
+		def mp(ammount):
+			addMana=0
+			mpMax = Player.Get.maxMp()
+
+			if ammount == 'full':
+				addMana = Player.Get.maxMp()
+
+			else:
+				addMana = Player.Get.mp() + ammount
+
+			if addMana < mpMax:
+				addMana = Player.Get.maxMp()
+
+			Player.Update.mp(addMana)
 
 	class Update:
 		global _UPDATE_
@@ -131,9 +181,10 @@ class Player:
 			saveName = 'Saves/'+nameVar+'.JDere'
 			with open(saveName, 'r+') as rawData:
 				data = json.load(rawData)
-				data[up] = value
 				rawData.seek(0)
-				json.dump(data, rawData, indent=2)
+				data[up] = value
+				json.dump(data, rawData, indent=1)
+				rawData.truncate()
 				rawData.close()
 
 		def local(upLocal):
@@ -203,4 +254,4 @@ class Player:
 			_UPDATE_('subWeapon2',subWeapon2Up)
 
 		def xp(xpUp):
-			_UPDATE_('xp',int(xpUp))
+			_UPDATE_('xp',xpUp)

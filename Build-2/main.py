f223a5c634475673b55e1fd5ccba950a6dd890a5
VoidTsundere <71775119+VoidTsundere@users.noreply.github.com>
2021-02-25 17:58:58 -0300

    Beta 0.0.0.0

--- Build-2/main.py
+++ Build-2/main.py
@@ -1,17 +1,40 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5.QtGui import QFont, QFontDatabase
+from PyQt5.QtGui import QFont, QFontDatabase, QKeyEvent
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore
 from sys import argv, exit
+import sqlite3
+
+conn = sqlite3.connect('data/data.dere')
 
 app = QApplication(argv)
 
 class game:
-	class mainWindow(QMainWindow):
+	class mainWindow(QWidget):
 		
 		def __init__(self):
-			QMainWindow.__init__(self)
+			super().__init__()
+
+			def runConsole():
+				command = self.dlg.console.text()
+
+				if 'exit' in command:
+					self.dlg.close()
+
+				if 'clear' in command:
+					self.dlg.textBox.clear()
+
+				if 'run' in command:
+					commandZone = command.split(' ')
+					if commandZone[1] == 'new':
+						if commandZone[2] == 'save':
+							try:
+								playerName = commandZone[3]
+							except:
+								self.dlg.textBox.append("Use 'run new save (player name)'")
+
+				self.dlg.console.clear()
 
 			screen = app.primaryScreen()
 			self.dlg = uic.loadUi('data/ui/main.ui')
@@ -25,14 +48,19 @@ class game:
 			self.dlg.console.installEventFilter(self)
 
 			self.dlg.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+			self.dlg.console.returnPressed.connect(runConsole)
 			self.dlg.showMaximized()
 
+			self.dlg.exit_button.clicked.connect(self.dlg.close)
+			self.dlg.exit_button.setFont(self.fontConsole)
+			self.dlg.exit_button.move(screen.size().width()-50,0)
+
 			self.dlg.show()
-			print(screen.size().width())
+		
 
-		def keyEvent(self, event):
-			print('defff')
-
+		def keyPressEvent(self, event):
+			if event.key() == QtCore.Qt.Key_Space:
+				printt()
 
 window = game.mainWindow()
 exit(app.exec_())
